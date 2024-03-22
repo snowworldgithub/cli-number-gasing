3e89b17031727fee8939d5d793eb993a3147ec0f
snowworldgithub <160354474+snowworldgithub@users.noreply.github.com>
2024-03-23 01:16:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a random number
+// 2) user input for gasing number
+// 3) compare user input with computer generated number and show result
+function getRandomInt(min, max) {
+    min = Math.ceil(min); // Round up the minimum value
+    max = Math.floor(max); // Round down the maximum value
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// Example usage:
+const randomNum = getRandomInt(1, 10); // Returns a random integer between 1 and 100 (inclusive)
+//console.log(randomNum); for your concept
+const guessnumber = await inquirer.prompt([
+    { name: "userGuessNumber", type: "number", message: "please guess a number 1-10" },
+]);
+if (guessnumber.userGuessNumber === randomNum) {
+    console.log("congratulation you guess right number");
+}
+else {
+    console.log("unfortunately you guess wrong number");
+}
+console.log(guessnumber);
